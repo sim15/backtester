f267d6572922b04d90cf6983c6283e345ffc056a
sim15 <sbeyzerov@gmail.com>
2023-05-12 12:26:27 -0400

    refactor data handling and processing

---
+++ build/CMakeFiles/back_test.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/back_test.dir/test/back_test.cpp.obj"
+  "CMakeFiles/back_test.dir/test/back_test.cpp.obj.d"
+  "back_test.exe"
+  "back_test.exe.manifest"
+  "back_test.pdb"
+  "back_test[1]_tests.cmake"
+  "libback_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/back_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
